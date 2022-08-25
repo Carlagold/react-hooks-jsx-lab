eaa763c180402cae6f0b4e55e275aacdde757043
Carla <csachett@student.scf.edu>
2022-08-24 23:58:56 -0400

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,14 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    <h2>About Me</h2>
+    <p>“Watch your thoughts, they become your words; watch your words, they become your actions; watch your actions, they become your habits; watch your habits, they become your character; watch your character, it becomes your destiny.”
+    ― Lao Tzu</p>
+    <img src={image} alt="I made this" />
+  </div>
+  );
 }
 
 export default About;
